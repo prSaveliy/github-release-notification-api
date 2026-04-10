542f1d38409604ad0b16e948d9b24f5167f5c0d8
prisyaga <papsavel@gmail.com>
2026-04-10 23:25:22 +0300

    feat: add global error handler and AppError interface

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,28 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyRequest, FastifyReply } from 'fastify';
 
 import fastifyEnv from '@fastify/env';
 
 import { envSchema } from './config/index.js';
+import { AppError } from './commons/interfaces/AppError.js';
 
 const buildApp = async () => {
   const app = Fastify({
     logger: true,
     trustProxy: 1,
   });
+
+  app.setErrorHandler(
+    (error: AppError, request: FastifyRequest, reply: FastifyReply) => {
+      reply.status(error.statusCode || 500).send({
+        message:
+          error.statusCode &&
+          (error.statusCode < 500)
+            ? error.message
+            : 'Internal server error',
+        details: error.details ?? '',
+      });
+    },
+  );
 
   // plugins
   await app.register(fastifyEnv, {
